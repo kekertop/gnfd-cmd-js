3711394bf19d49a3ed8e577d397458b0a805681a
Jack0O7 <dastan.nomad@gmail.com>
2023-05-28 05:46:12 +0600

    payment module fixed

--- modules/cmd_payment.ts
+++ modules/cmd_payment.ts
@@ -1,39 +1,54 @@
 // createBucket send the create bucket request to storage provider
 import { getBucketNameByUrl } from "./utils";
-import { Client } from "@bnb-chain/greenfield-chain-sdk";
+import { newClient } from "./client";
+import { ConfigService } from "./config";
 import { VisibilityType } from "@bnb-chain/greenfield-cosmos-types/greenfield/storage/common";
+import {
+  executeTransaction,
+  getPrimaryStorageProviderInfo,
+} from "../utils/transactionUtils";
 
-const client = Client.create(
-  "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org",
-  "5600"
-);
+class PaymentService {
+  public async buyQuotaForBucket(
+      bucketUrl: string,
+      chargeQuotaFlag?: number
+  ) {
+    const client = await newClient();
+    const config = await ConfigService.getInstance().getConfig();
 
-const PUBLIC_KEY = "0x54c4F86eFfcecf4A06E70C262f1b762764ee7265";
-const PRIVATE_KEY =
-  "0x9b9ef687e5f9d36f3f411c1a13af0993c01b75c729df2dcb5c75782f7e0e2045";
+    const spInfo = await client.sp.getStorageProviders();
+    let buyQuotaTx
+    try {
+      buyQuotaTx = await client.bucket.getBucketReadQuota({
+        bucketName: getBucketNameByUrl(bucketUrl),
+        endpoint: spInfo[0].endpoint,
+      })
+    } catch(ex) {
+      throw new Error("Unable to fetch readquota for bucket")
+    }
+    console.log(
+        `Successfully found quota to buy "${buyQuotaTx}".`
+    );
+  }
 
-export async function buyQuotaForBucket(
-  bucketUrl: string,
-  chargeQuotaFlag?: number
-) {
-  console.log(
-    await client.bucket.getBucketReadQuota({
-      bucketName: "uniqueforrefefesefal",
-      endpoint: "https://gnfd-testnet-sp-2.bnbchain.org",
-    })
-  );
+  public async getQuotaInfo(
+      bucketUrl: string
+  ) {
+    const client = await newClient();
+    const config = await ConfigService.getInstance().getConfig();
+
+    const spInfo = await client.sp.getStorageProviders();
+    let getQuotaInfoTx
+    try {
+        getQuotaInfoTx = await client.bucket.getBucketReadQuota({
+        bucketName: getBucketNameByUrl(bucketUrl),
+        endpoint: spInfo[0].endpoint,
+      });
+    } catch(ex) {
+      throw new Error("Unable to fetch readquota for bucket")
+    }
+    console.log(
+        `Successfully found quota ${getQuotaInfoTx}".`
+    );
+  }
 }
-
-export async function getQuotaInfo(bucketUrl: string) {
-  const spInfo = await client.sp.getStorageProviders();
-  const transferTx = await client.bucket.getBucketReadQuota({
-    bucketName: getBucketNameByUrl(bucketUrl),
-    endpoint: spInfo[0].endpoint,
-  });
-  console.log(transferTx);
-}
-
-module.exports = {
-  buyQuotaForBucket,
-  getQuotaInfo,
-};
