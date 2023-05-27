24771e4d803854bbf1733b30a592da0ee50a49c8
Jack0O7 <dastan.nomad@gmail.com>
2023-05-28 01:29:09 +0600

    cmd_account module functions added

--- modules/cmd_account.ts
+++ modules/cmd_account.ts
@@ -1,50 +1,121 @@
-function createPaymentAccount() {
+import {Client} from "@bnb-chain/greenfield-chain-sdk";
 
+const client = Client.create(
+    "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org",
+    "5600"
+);
+
+const PUBLIC_KEY = "0x54c4F86eFfcecf4A06E70C262f1b762764ee7265"
+const PRIVATE_KEY = "0x9b9ef687e5f9d36f3f411c1a13af0993c01b75c729df2dcb5c75782f7e0e2045"
+const DEFAULT_ACCOUNT = "0x22ED18D6c79f91ba186b23f1920Df4a2f2Ca1E3B"
+
+export async function createPaymentAccount() {
+    const transferTx = await client.account.createPaymentAccount({
+        creator: PUBLIC_KEY
+    });
+    const simulateInfo = await transferTx.simulate({
+        denom: 'BNB',
+    });
+    console.log("check")
+    const broadcastRes = await transferTx.broadcast({
+        denom: 'BNB',
+        gasLimit: Number(simulateInfo.gasLimit),
+        gasPrice: simulateInfo.gasPrice,
+        payer: PUBLIC_KEY,
+        granter: '',
+        privateKey: PRIVATE_KEY,
+    });
+    console.log(broadcastRes.transactionHash)
 }
 
 
-function deposit(
-    toAddressFlag?: string,
-    amountFlag?: string,
+export async function deposit(
+    toAddressFlag: string,
+    amountFlag: string,
 ) {
-
+    const transferTx = await client.payment.deposit({
+        creator: PUBLIC_KEY,
+        to: toAddressFlag,
+        amount: amountFlag,
+    });
+    const simulateInfo = await transferTx.simulate({
+        denom: 'BNB',
+    });
+    const broadcastRes = await transferTx.broadcast({
+        denom: 'BNB',
+        gasLimit: Number(simulateInfo.gasLimit),
+        gasPrice: simulateInfo.gasPrice,
+        payer: PUBLIC_KEY,
+        granter: '',
+        privateKey: PRIVATE_KEY,
+    });
 }
 
 
-function withdraw(
-    fromAddressFlag?: string,
-    amountFlag?: string,
+export async function withdraw(
+    fromAddressFlag: string,
+    amountFlag: string,
 ) {
-
+    const transferTx = await client.payment.withdraw({
+        creator: PUBLIC_KEY,
+        from: fromAddressFlag,
+        amount: amountFlag,
+    })
+    const simulateInfo = await transferTx.simulate({
+        denom: 'BNB',
+    });
+    const broadcastRes = await transferTx.broadcast({
+        denom: 'BNB',
+        gasLimit: Number(simulateInfo.gasLimit),
+        gasPrice: simulateInfo.gasPrice,
+        payer: PUBLIC_KEY,
+        granter: '',
+        privateKey: PRIVATE_KEY,
+    });
 }
 
 
-function listPaymentAccounts(
-    ownerAddr: string,
+export async function listPaymentAccounts(
     ownerAddressFlag?: string,
 ) {
-
+    const transferTx = await client.account.getPaymentAccountsByOwner(ownerAddressFlag || DEFAULT_ACCOUNT);
+    console.log(transferTx.paymentAccounts)
 }
 
-function getAccountBalance(
+export async function getAccountBalance(
     addressFlag?: string,
 ) {
-
+    const transferTx = await client.account.getAccountBalance({
+        address: addressFlag || DEFAULT_ACCOUNT,
+        denom: 'BNB'
+    });
+    console.log(transferTx.balance?.amount)
 }
 
 
-function transfer(
-    toAddressFlag?: string,
-    amountFlag?: string,
+export async function transfer(
+    toAddressFlag: string,
+    amountFlag: string,
 ) {
-
-}
-
-module.exports = {
-    createPaymentAccount: createPaymentAccount,
-    deposit: deposit,
-    withdraw: withdraw,
-    listPaymentAccounts: listPaymentAccounts,
-    getAccountBalance: getAccountBalance,
-    transfer: transfer,
+    const transferTx = await client.account.transfer({
+        fromAddress: PUBLIC_KEY,
+        toAddress: toAddressFlag,
+        amount: [
+            {
+                denom: 'BNB',
+                amount: amountFlag,
+            },
+        ],
+    });
+    const simulateInfo = await transferTx.simulate({
+        denom: 'BNB',
+    });
+    const broadcastRes = await transferTx.broadcast({
+        denom: 'BNB',
+        gasLimit: Number(simulateInfo.gasLimit),
+        gasPrice: simulateInfo.gasPrice,
+        payer: PUBLIC_KEY,
+        granter: '',
+        privateKey: PRIVATE_KEY,
+    });
 }
