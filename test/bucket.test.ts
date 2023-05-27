ded05f639e6599ce4a2a410899aeb92dc654843b
Zhan <zaubakirov@beeline.kz>
2023-05-28 05:12:29 +0600

    remove unnecessary files

--- test/bucket.test.ts
+++
@@ -1,9 +0,0 @@
-import {createBucket, listBuckets, updateBucket, deleteBucket} from "../modules/bucket";
-
-
-test("do it", async () => {
-    //let response = await createBucket("gnfd://teassst3")
-    //let response = await updateBucket("gnfd://teassst3", undefined, 0, "VISIBILITY_TYPE_PUBLIC_READ")
-    //let response = await listBuckets()
-    let response = await deleteBucket("gnfd://teassst3")
-})
