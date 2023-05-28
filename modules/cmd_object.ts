41940f71df7e3c4ce36410a09134d12f96c21d2a
VeyelutD <daulet130694@gmail.com>
2023-05-28 12:28:41 +0600

    added new object scripts and updated utils

--- modules/cmd_object.ts
+++ modules/cmd_object.ts
@@ -1,72 +1,161 @@
-function putObject(
-    filePath: string, //ArgsUsage: "[filePath] OBJECT-URL"
-    secondarySPFlag?: string,
-    contentTypeFlag?: string,
-    visibilityFlag?: any,
-    folderFlag?: string,
-) {
+import { Client } from "@bnb-chain/greenfield-chain-sdk";
+import fs from "fs";
+import { VisibilityType } from "@bnb-chain/greenfield-cosmos-types/greenfield/storage/common";
+import { ConfigService } from "./config";
+import { newClient } from "./client";
+import { executeTransaction } from "../utils/transactionUtils";
+import {
+  getBucketNameByUrl,
+  getObjAndBucketNames,
+  parseBucketAndObject,
+} from "./utils";
+const client = Client.create(
+  "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org",
+  "5600"
+);
 
-}
+function putObject(
+  filePath: string, //ArgsUsage: "[filePath] OBJECT-URL"
+  secondarySPFlag?: string,
+  contentTypeFlag?: string,
+  visibilityFlag?: any,
+  folderFlag?: string
+) {}
 
 function putObjectPolicy(
-    objectUrl: string, //ArgsUsage: "[filePath] OBJECT-URL"
-    groupIDFlag?: number,
-    granteeFlag?: string,
-    actionsFlag?: any,
-    effectFlag?: string,
-    expireTimeFlag?: number,
-) {
+  objectUrl: string, //ArgsUsage: "[filePath] OBJECT-URL"
+  groupIDFlag?: number,
+  granteeFlag?: string,
+  actionsFlag?: any,
+  effectFlag?: string,
+  expireTimeFlag?: number
+) {}
 
+async function getObject(
+  filePath: string,
+  objectUrl: string, //ArgsUsage: "[filePath] OBJECT-URL"
+  startOffsetFlag?: number,
+  endOffsetFlag?: number
+) {
+  const { bucketName, objectName } = parseBucketAndObject(objectUrl);
+  const headObject = await client.object.headObject(bucketName, objectName);
+  if (!headObject.objectInfo) {
+    throw new Error("Object was not found");
+  }
+  // If file exist, open it in append mode
+  try {
+    fs.readFileSync(filePath, { flag: "append" });
+  } catch (e) {
+    throw new Error(e);
+  }
+  // flag has been set
+  if (startOffsetFlag !== 0 || endOffsetFlag !== 0) {
+  }
+  const getTx = client.object.getObject({ bucketName, objectName });
+  console.log(
+    `download object ${objectName} successfully, the file path is ${filePath}`
+  );
 }
 
-function getObject(
-    filePath: string, //ArgsUsage: "[filePath] OBJECT-URL"
-    startOffsetFlag?: number,
-    endOffsetFlag?: number,
-) {
-
+async function cancelCreateObject(objectUrl: string) {
+  const config = await ConfigService.getInstance().getConfig();
+  const { bucketName, objectName } = parseBucketAndObject(objectUrl);
+  try {
+    const tx = await client.object.cancelCreateObject({
+      bucketName,
+      objectName,
+      operator: config.publicKey,
+    });
+    const response = await executeTransaction(tx);
+    console.log(
+      `Successfully cancelled object creation". Transaction: ${response.transactionHash}`
+    );
+  } catch (ex) {
+    throw new Error("Unable to cancel object creation");
+  }
 }
 
-function cancelCreateObject(
-    objectUrl: string,
-) {
-
+async function listObjects(bucketUrl: string) {
+  const bucketName = getBucketNameByUrl(bucketUrl);
+  const knownBucket = await client.bucket.headBucket(bucketName);
+  const storageProvider = await client.sp.getStorageProviderInfo(
+    knownBucket.bucketInfo.primarySpAddress
+  );
+  const tx = await client.object.listObjects({
+    bucketName,
+    endpoint: storageProvider.endpoint,
+  });
+  console.log(tx);
 }
 
-function listObjects(
-    bucketUrl: string,
+async function createFolder(
+  objectUrl: string,
+  visibilityFlag?: any,
+  objectPrefix?: string
 ) {
+  let { bucketName, objectName } = getObjAndBucketNames(objectUrl);
+  const client = await newClient();
+  const config = await ConfigService.getInstance().getConfig();
 
+  if (
+    !visibilityFlag ||
+    Object.values(VisibilityType).includes(visibilityFlag)
+  ) {
+    throw new Error("Visibility is set to a value that is not allowed");
+  }
+  objectName = objectName + "/";
+
+  const prefix = objectPrefix;
+  if (prefix) {
+    objectName = prefix + "/" + objectName;
+  }
+  try {
+    let createFolderTx = await client.object.createFolder({
+      bucketName,
+      creator: config.publicKey,
+      expectSecondarySpAddresses: [],
+      objectName,
+      spInfo: {
+        endpoint: "",
+        sealAddress: "",
+        secondarySpAddresses: [],
+      },
+      file: new File([""], "file.txt"),
+    });
+
+    const response = await executeTransaction(createFolderTx);
+
+    console.log(
+      `Successfully created folder". Transaction: ${response.transactionHash}`
+    );
+  } catch (ex) {
+    throw new Error("Unable to create folder");
+  }
 }
 
-function createFolder(
-    objectUrl: string,
-    visibilityFlag?: any,
-    objectPrefix?: string,
-) {
+async function updateObject(objectUrl: string, visibilityFlag?: any) {
+  const { bucketName, objectName } = parseBucketAndObject(objectUrl);
 
+  if (!visibilityFlag) {
+    throw new Error("Visibility must be specified");
+  }
+
+  const objectInfo = await client.object.headObject(bucketName, objectName);
 }
 
-function updateObject(
-    objectUrl: string,
-    visibilityFlag?: any,
-) {
-
-}
-
-function getUploadInfo(
-    objectUrl: string,
-) {
-
+async function getUploadInfo(objectUrl: string) {
+  const { bucketName, objectName } = getObjAndBucketNames(objectUrl);
+  const client = await newClient();
+  const config = await ConfigService.getInstance().getConfig();
 }
 
 module.exports = {
-    putObject: putObject,
-    putObjectPolicy: putObjectPolicy,
-    getObject: getObject,
-    cancelCreateObject: cancelCreateObject,
-    listObjects: listObjects,
-    createFolder: createFolder,
-    updateObject: updateObject,
-    getUploadInfo: getUploadInfo,
-}
+  putObject: putObject,
+  putObjectPolicy: putObjectPolicy,
+  getObject: getObject,
+  cancelCreateObject: cancelCreateObject,
+  listObjects: listObjects,
+  createFolder: createFolder,
+  updateObject: updateObject,
+  getUploadInfo: getUploadInfo,
+};
--- modules/utils.ts
+++ modules/utils.ts
@@ -1,38 +1,48 @@
 import * as url from "url";
+import {} from "@bnb-chain/greenfield-chain-sdk";
 
-export function parseBucketAndObject(urlPath: string): [string, string] | null {
-    if (urlPath.includes("gnfd://")) {
-        urlPath = urlPath.replace("gnfd://", "");
-    }
+function getPassword(config: string, passwordFileFlag?: string) {}
 
-    const index = urlPath.indexOf("/");
-    if (index <= -1) {
-        return null;
-    }
-
-    const bucketName = urlPath.substring(0, index);
-    const objectName = urlPath.substring(index + 1);
-
-    return [bucketName, objectName];
+function getBucketNameByUrl(urlInfo: string): string {
+  if (urlInfo.includes("gnfd://")) {
+    urlInfo = urlInfo.slice("gnfd://".length);
+  }
+  const splits = urlInfo.split("/", 1);
+  return splits[0];
 }
 
+function getGroupNameByUrl(urlInfo: string) {}
 
-export function getBucketNameByUrl (
-    urlInfo: string,
-) : string {
-    if (urlInfo.includes("gnfd://")) {
-        urlInfo = urlInfo.slice("gnfd://".length);
-    }
-    const splits = urlInfo.split("/", 1);
-    return splits[0];
-}
+function parseActions(actionsFlag?: string, isObjectPolicy?: boolean) {}
 
-export function getGroupNameByUrl(
-    urlInfo: string,
-) : string {
-    if (urlInfo.includes("gnfd://")) {
-        urlInfo = urlInfo.slice("gnfd://".length);
-    }
-    const splits = urlInfo.split("/", 1);
-    return splits[0];
+function parseBucketAndObject(urlPath: string) {
+  if (urlPath.includes("gnfd://")) {
+    urlPath = urlPath.slice("gnfd://".length);
+  }
+
+  const index = urlPath.indexOf("/");
+
+  if (index < 0) {
+    throw new Error("url not right, can not parse bucket name and object name");
+  }
+
+  return {
+    bucketName: urlPath.slice(0, index),
+    objectName: urlPath.slice(index + 1),
+  };
 }
+function getObjAndBucketNames(urlInfo: string) {
+  const { bucketName, objectName } = parseBucketAndObject(urlInfo);
+  if (!bucketName || !objectName) {
+    throw new Error("fail to parse bucket name or object name");
+  }
+  return { bucketName, objectName };
+}
+export {
+  getPassword,
+  getBucketNameByUrl,
+  getGroupNameByUrl,
+  parseActions,
+  parseBucketAndObject,
+  getObjAndBucketNames,
+};
